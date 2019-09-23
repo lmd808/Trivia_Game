080e25380206313b4c3e2dd82d9a32841ec59839
limabean <lditom808@gmail.com>
2019-09-23 10:00:33 -0400

    addition of original files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Trivia_Game
 This is my trivia Game 
+
+# Game Play 
+	player opens site 
+	button says click to play 
+	div clears and loads first question 
+	after div clears button are pushed containing anwsers 
+	from a random array 
+	
---
+++ javascript/game.js
@@ -0,0 +1,42 @@
+//variables
+//my wins
+wins = 0;
+// my loses
+loses = 0;
+// my guesses
+guessesLeft = 2;
+// my questions array
+myQUestionsArray = [];
+
+// intial button click
+
+// intial button click loads a RANDOMLY selected question object
+
+// i have ten of these objects
+function Questions(question, correctAns, wrongAns1, wrongAns2, wrongAns3) {
+	//name of character
+	this.question = question;
+	// health points
+	this.correctAns = c1;
+	// attack power
+	this.wrongAns1 = w1;
+	// counter atack power
+	this.wrongAns2 = w2;
+	// skills
+	this.wrongAns4 = w3;
+}
+
+// they will bebuilt off of my question prototype object model
+
+// questions
+
+// they will be the first thing i develop
+
+// format for checking them
+
+//- if ... click corect anwser
+//wins++
+// else
+// loses++
+
+//
